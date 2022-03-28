bbad30cd58b82a4d34288b1d7018eb458b41a392
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2022-03-28 11:39:15 -0400

    landing page almost finished

---
+++ sarahmuchomusic/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="Music-app">
+      <h1>Sarah Mucho</h1>
+    </div>
+  );
+}
+
+export default App;
